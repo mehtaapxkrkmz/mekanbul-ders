8bd8c77504e8da6a9b857658ec23a2ead5aefadc
Mehtap <129941442+mehtaapxkrkmz@users.noreply.github.com>
2024-12-26 19:20:25 +0300

    home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,67 +1,19 @@
 import InputWithLabel from "./InputWithLabel";
 import VenueList from "./VenueList";
 import Header from "./Header";
-<<<<<<< HEAD
-import React, { useState } from "react";
-import VenueDataService from "../services/VenueDataService"
-import {useSelector,useDispatch} from "react-redux";
-import { useParams } from "react-router-dom";
-
-
-const Home = () => {
-  const dispatch=useDispatch();
-  const venues=useSelector((state) => state.data);
-  const isError=useSelector((state) =>state.isError);
-  const isLoading =useSelector((state) =>state.isLoading);
-  const isSuccess = useSelector((state) =>state.isSuccess);
-  const [coordinate,setCoordinate] =useState({ lat:1, long:1,});
-  
- 
-  const [searchVenue,setSearchVenue]=useState("");
-  const search=(event)=>{
-    setSearchVenue(event.target.value);
-  };
-  const filteredVenues = Array.isArray(venues) ? venues.filter(
-(venue) =>
-  venue.name.toLowerCase().includes(searchVenue.toLowerCase()) ||
-  venue.address.toLowerCase().includes(searchVenue.toLowerCase())
-  ) : [];
-
-
-   //Şimdilik veri statik. Backend bitince Rest API'den gelecek.
-
-   React.useEffect(() =>{
-    if("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position){
-        setCoordinate({
-          lat:position.coords.latitude,
-          long:position.coords.longitude,
-        });
-      });
-    }
-   },[]);
-  
-  React.useEffect(()=>{
-    dispatch({type:"FETCH_INIT"});
-    VenueDataService.nearbyVenues(coordinate.lat,coordinate.long)
-    .then(function(response) {
-      dispatch({type:"FETCH_SUCCESS",payload:response.data});
-    }).catch(function(){
-      dispatch({type:"FETCH_FAILURE"});
-    })},[coordinate.lat,coordinate.long] );
-  
-=======
 import React from "react";
-import VenueDataService from "../services/VenueDataService";
 import { useSelector, useDispatch } from "react-redux";
+import VenueDataService from "../services/VenueDataService";
 const Home = () => {
-  //Şimdilik veri statik. Backend bitince Rest API'den gelecek.
-  const [coordinate, setCoordinate] = React.useState({ lat: 1, long: 1 });
+  const [coordinate, setCoordinate] = React.useState({
+    lat: 1,
+    long: 1,
+  });
   const dispatch = useDispatch();
   const venues = useSelector((state) => state.data);
   const isError = useSelector((state) => state.isError);
-  const isSuccess = useSelector((state) => state.isSuccess);
   const isLoading = useSelector((state) => state.isLoading);
+  const isSuccess = useSelector((state) => state.isSuccess);
 
   React.useEffect(() => {
     if ("geolocation" in navigator) {
@@ -80,12 +32,25 @@ const Home = () => {
       .then(function (response) {
         dispatch({ type: "FETCH_SUCCESS", payload: response.data });
       })
-      .catch(() => {
+      .catch(function () {
         dispatch({ type: "FETCH_FAILURE" });
       });
   }, [coordinate.lat, coordinate.long]);
+  //üstteki konumu sürekli güncellesin diye var
 
->>>>>>> e4cd6635d4be8f5250f7a9c9056f494946d4af22
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+
+  const filteredVenues = venues.filter((venue) =>
+    venue.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+
   return (
     <div>
       <Header
@@ -97,35 +62,18 @@ const Home = () => {
         label="Mekan Ara:"
         type="text"
         isFocused
-        onInputChange={search}
-        value={searchVenue}
+        onInputChange={handleSearchInputChange}
+        value={searchTerm}
       />
       <hr />
       <div className="row">
-<<<<<<< HEAD
-        {isError ?(
-          <p>Hata var .</p>
-        ):isLoading?(
-          <p>Mekanlar yükleniyor..</p>
-        ):(
-          isSuccess && (
-            <div className="row">
-           <VenueList venues={filteredVenues}/>  
-           </div> 
-          )
-        )}
-
-=======
         {isError ? (
-          <p>Veri alınamadı</p>
+          <p>Hata var.</p>
         ) : isLoading ? (
-          <p>Veriler yükleniyor</p>
+          <p>Mekanlar Yükleniyor...</p>
         ) : (
-          isSuccess && (
-            <VenueList venues={Array.isArray(venues) ? venues : []} />
-          )
+          isSuccess && <VenueList venues={filteredVenues} />
         )}
->>>>>>> e4cd6635d4be8f5250f7a9c9056f494946d4af22
       </div>
     </div>
   );
