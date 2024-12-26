210935e7bc68ca533bc105a6a2746db733c60928
Mehtap <129941442+mehtaapxkrkmz@users.noreply.github.com>
2024-12-26 19:19:31 +0300

    venuedetail

--- frontend/src/components/VenueDetail.jsx
+++ frontend/src/components/VenueDetail.jsx
@@ -6,37 +6,19 @@ import HourList from "./HourList";
 import CommentList from "./CommentList";
 import React from "react";
 import { useParams } from "react-router-dom";
-<<<<<<< HEAD
-import { useSelector,useDispatch } from "react-redux";
-import VenueDataService from "../services/VenueDataService";
-const VenueDetail = () => {
-  const { id } = useParams();
-  const dispatch=useDispatch();
-  const venue=useSelector(state=>state.data);
-
-  React.useEffect(()=>{
-    dispatch({type:"FETCH_INIT"});
-    VenueDataService.getVenue(id)
-    .then((response)=>{
-      dispatch({type:"FETCH_SUCCESS",payload:response.data});
-    }).catch(()=>{
-      dispatch({type:"FETCH_FAILURE"});
-    });
-  },[id]);
-=======
-import VenueDataService from "../services/VenueDataService";
 import { useSelector, useDispatch } from "react-redux";
+import VenueDataService from "../services/VenueDataService";
+
 const VenueDetail = () => {
   const { id } = useParams();
-  const dispatch=useDispatch();
-  const venue=useSelector((state)=>state.data);
-  React.useEffect(()=>{
-    dispatch({ type: "FETCH_INIT" });
-    VenueDataService.getVenue(id).then(function(response){
+  const dispatch = useDispatch();
+  const venue = useSelector((state) => state.data);
+  React.useEffect(() => {
+    VenueDataService.getVenue(id).then((response) => {
       dispatch({ type: "FETCH_SUCCESS", payload: response.data });
     });
-  },[]);
->>>>>>> e4cd6635d4be8f5250f7a9c9056f494946d4af22
+  }, [id]);
+
   return (
     <div>
       <Header headerText={venue.name} />
@@ -54,11 +36,7 @@ const VenueDetail = () => {
                     <h2 className="panel-title ">Açılış Saatleri</h2>
                   </div>
                   <div className="panel-body ">
-<<<<<<< HEAD
                     <HourList hourList={venue.hours || []} />
-=======
-                    <HourList hourList={venue.hours ||[]} />
->>>>>>> e4cd6635d4be8f5250f7a9c9056f494946d4af22
                   </div>
                 </div>
                 <div className="panel panel-primary">
@@ -66,11 +44,7 @@ const VenueDetail = () => {
                     <h2 className="panel-title ">Neler Var?</h2>
                   </div>
                   <div className="panel-body ">
-<<<<<<< HEAD
                     <FoodAndDrinkList foodAndDrinkList={venue.foodanddrink || []} />
-=======
-                    <FoodAndDrinkList foodAndDrinkList={venue.foodanddrink ||[]} />
->>>>>>> e4cd6635d4be8f5250f7a9c9056f494946d4af22
                   </div>
                 </div>
               </div>
@@ -83,11 +57,9 @@ const VenueDetail = () => {
                     <img
                       className="img img-responsive img-rounded"
                       alt="Konum Bilgisi"
-                      src={`https://maps.googleapis.com/maps/api/staticmap?center=${
-                        venue.coordinates
-                      }&zoom=12&size=600x400&markers=${
-                        venue.coordinates
-                      }&key=AIzaSyCmmKygTpBzHGOZEciJpAdxC9v_tWHagnE`}
+                      src={`https://maps.googleapis.com/maps/api/staticmap?center=${venue.coordinates
+                        }&zoom=12&size=600x400&markers=${venue.coordinates
+                        }&key=AIzaSyCmmKygTpBzHGOZEciJpAdxC9v_tWHagnE`}
                     />
                   </div>
                 </div>
@@ -100,18 +72,14 @@ const VenueDetail = () => {
                 <div className="panel-heading ">
                   <NavLink
                     className="btn btn-default pull-right"
-                    to={`/venue/${id}/comment/new`}state={{ name: venue.name }}
+                    to={`/venue/${id}/comment/new`} state={{ name: venue.name }}
                   >
                     Yorum Ekle{" "}
                   </NavLink>
                   <h2 className="panel-title ">Yorumlar</h2>
                 </div>
                 <div className="panel-body ">
-<<<<<<< HEAD
                   <CommentList commentList={venue.comments || []} />
-=======
-                  <CommentList commentList={venue.comments ||[]} />
->>>>>>> e4cd6635d4be8f5250f7a9c9056f494946d4af22
                 </div>
               </div>
             </div>
