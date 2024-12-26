89c683986ee22dc68b94c80e88171a6a2037f53e
Mehtap <129941442+mehtaapxkrkmz@users.noreply.github.com>
2024-12-26 19:13:14 +0300

    baseurl

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://mekanbul-ders.vercel.app/api",
+  baseURL: "https://mekanbul-ashy.vercel.app/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
