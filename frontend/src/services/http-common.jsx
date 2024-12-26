ddf42f68f3b0b8375dd44c0e142234cfbcd4a4ef
Mehtap <129941442+mehtaapxkrkmz@users.noreply.github.com>
2024-12-26 19:02:51 +0300

    api

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://mekanbul-ders.vercel.app/api",
+  baseURL: "https://localhost:3000/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
